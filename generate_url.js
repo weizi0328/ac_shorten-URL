62ef275bc3d62d77d7c862abee923080ed27de07
weizi0328 <ankewang26@gmail.com>
2022-08-07 17:06:55 +0800

    feat: add copy url button

--- generate_url.js
+++
@@ -1,9 +0,0 @@
-const random = (length = 5) => {
-  let chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
-
-  let str = ''
-  for (let i = o; i < length; i++) {
-    str += chars.charAt(Math.floor(Math.random() * chars.length))
-  }
-  return str
-}
